9081bb3a1c6071cb5d82cc2bb91021aa0fccddc7
PedroDev0 <pedronerydev@gmail.com>
2025-02-04 18:45:16 -0300

    Permitindo panel ng do versel realizar requisição

--- src/main/java/com/pedro/painelsrvspring/config/WebConfig.java
+++ src/main/java/com/pedro/painelsrvspring/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://painel-ng.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
